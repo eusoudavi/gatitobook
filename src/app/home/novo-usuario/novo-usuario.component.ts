ea4542cce633f92e597c05e7a507be787897b863
davi.visintainer <davi.visintainer@placeti.com.br>
2022-12-06 15:29:10 -0300

    cadastrando novo usuario

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
@@ -12,37 +13,39 @@ import { usuarioSenhaiguaisValidator } from './usuario-senha-iguais.validator';
   styleUrls: ['./novo-usuario.component.css']
 })
 export class NovoUsuarioComponent {
-
   novoUsuarioForm!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private novoUsuarioService: NovoUsuarioService,
-    private usuarioExisteService: UsuarioExisteService
+    private usuarioExisteService: UsuarioExisteService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: ['', [
-        Validators.required,
-        Validators.email
-      ]],
-      fullName: ['', [
-        Validators.required,
-        Validators.minLength(4)
-      ]],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
       userName: ['', [minusculoValidator], [this.usuarioExisteService.usuarioJaExistente()]],
       password: ['']
-    }, {
-      validators: [usuarioSenhaiguaisValidator]
-      // ATENÇÃO: ESSE VALIDATORS TEM QUE SER TUDO MINÚSCULO
-    }
-    );
+    },
+      {
+        validators: [usuarioSenhaiguaisValidator]
+        // ATENÇÃO: ESSE VALIDATORS TEM QUE SER TUDO MINÚSCULO
+      });
   }
 
   cadastrar() {
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario);
+    if (this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(() => {
+        this.router.navigate(['']);
+      },
+        (error) => {
+          console.error(error)
+        }
+      );
+    }
   }
 
 }
