7bac88d432d3849a269d947088d74f9f921f8d83
davi.visintainer <davi.visintainer@placeti.com.br>
2022-12-06 11:14:43 -0300

    Formulário Assíncrono

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,8 +1,9 @@
-import { NovoUsuarioService } from './novo-usuario.service';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NovoUsuario } from './novo-usuario';
 import { minusculoValidator } from './minusculo.validator';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
+import { UsuarioExisteService } from './usuario-existe.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -13,7 +14,11 @@ export class NovoUsuarioComponent {
 
   novoUsuarioForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExisteService: UsuarioExisteService
+  ) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
@@ -25,7 +30,7 @@ export class NovoUsuarioComponent {
         Validators.required,
         Validators.minLength(4)
       ]],
-      userName: ['', [minusculoValidator]],
+      userName: ['', [minusculoValidator], [this.usuarioExisteService.usuarioJaExistente()]],
       password: ['']
     });
   }
--- src/app/home/novo-usuario/novo-usuario.service.ts
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -9,7 +9,10 @@ export class NovoUsuarioService {
 
   constructor(private httpCLiente: HttpClient) { }
 
-  cadastraNovoUsuario(novoUsuario: NovoUsuario){
+  cadastraNovoUsuario(novoUsuario: NovoUsuario) {
     return this.httpCLiente.post('http://localhost:3000/user/signup', novoUsuario)
+  }
+  verificaUsuarioExistente(nomeUsuario: string) {
+    return this.httpCLiente.get(`http://localhost:3000/user/exists/${nomeUsuario}`)
   }
 }
