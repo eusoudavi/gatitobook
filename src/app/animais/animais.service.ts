525309a9092a25063ab3b7c3724ca83ec665514f
davi.visintainer <davi.visintainer@placeti.com.br>
2022-12-19 15:30:43 -0300

    criação de comentários

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,23 +1,39 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, mapTo } from 'rxjs/operators';
 import { TokenService } from '../autenticacao/token.service';
 import { Animais as Animals, Animal } from './animais';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnimaisService {
-  constructor(private http: HttpClient, private tokenService: TokenService) {}
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
 
   listaDoUsuario(nomeDoUsuario: string): Observable<Animals> {
     return this.http.get<Animals>(`${API}/${nomeDoUsuario}/photos`);
   }
 
-  findById(id: number): Observable<Animal>{
+  findById(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`)
   }
+
+  excluirAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`)
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.http
+    .post(`${API}/photos/${id}/like`, {}, { observe: 'response'})
+    .pipe(mapTo(true), catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      )
+  }
+
 }
